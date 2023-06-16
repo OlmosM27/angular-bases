90eef709418e7793553db59388f403a985c59231
Martín Olmos <imec.olmosm24@gmail.com>
2023-06-16 11:40:44 -0700

    Commit para subir

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { CounterModule } from './counter/counter.module';
 import { HeroesModule } from './heroes/heroes.module';
 
 import { AppComponent } from './app.component';
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
   declarations: [
@@ -12,8 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    DbzModule,
     CounterModule,
-    HeroesModule
+    HeroesModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
